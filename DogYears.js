73440dc954d284060baa76f7fb4cdb60f5e8ab9e
unknown <you@example.com>
2021-01-26 21:48:51 -0700

    initial commit

---
+++ DogYears.js
@@ -0,0 +1,26 @@
+//Declaring Variable My Age
+const myAge = 5;
+
+//Declaring Variable
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+//Subtracting 2 years from myAge
+let laterYears = myAge - 2 ;
+
+//Calculating the Number of Dog Years
+laterYears *= 4;
+
+//Print values to the console
+console.log(earlyYears);
+console.log(laterYears);
+
+//My Dog Age in Dog Years
+let myAgeInDogYears = earlyYears + laterYears;
+
+//My Name
+myName = 'Sally'.toLowerCase();
+
+//Sentence telling my name, human age, and dog age
+console.log(`My name is ${myName}. I am ${myAge} years old in ${myAgeInDogYears} human years which is years old in dog years.`);
